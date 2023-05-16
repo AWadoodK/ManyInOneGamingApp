f9f55c64ff6e72e492a6d5264566a7c06e6ea4a6
Azhar20afif <124029366+Azhar20afif@users.noreply.github.com>
2023-05-16 05:55:51 +0500

    Improved wordle

--- wordlyazhar.cpp
+++ wordlyazhar.cpp
@@ -5,17 +5,38 @@
 using namespace std;
 int score=0;
 int lives=5;
+int lvlupdate = 1;
+string name;
+void introline()
+{
+    for(int i=0;i<139;i++)
+    {
+        cout<<"-";
+    }
+}
+void leveldisplay()
+{
+   
+    cout << "*************" <<endl;
+    cout << "*           *" <<endl;
+    cout << "*  Welcome  *" <<endl;
+    cout << "*     to    *" <<endl;
+    cout << "*   Level "<<lvlupdate<<" *"<<endl;
+    cout << "*           *" <<endl;
+    cout << "*************" <<endl;
+}
 class Game
 {
     private:
     
-    string words[2];
+    string words[10];
     protected:
     int correct;   //This is your right answers
     int total;     //This is your number of words that you have to guess
     int limit;     // Used as a check mark when correct == limit
     public:
     friend class LV1;
+    friend class LV2;
     Game()
     {}
     Game(int x,int y,int z, string *arr)
@@ -38,7 +59,7 @@ class Game
             cout << "Total words you have to guess: " << total << endl;
             cout<<"Total lives: "<<lives<<endl;
             cout << "Enter a word formed by these letters" << endl;
-        //    cout << "(1) a\t(2) t\t(3) l\t(4) e" << endl;     REMOVE LATER
+        
             cout<<words[0][2]<<" "<<words[0][3]<<" "<<words[0][0]<<" "<<words[0][1]<<endl;
             
             cin >> guess;
@@ -50,16 +71,17 @@ class Game
                     total--;
                     match = true;
                     score++;
-                    cout<<"Nice you got that right\n";
+                    cout<<"Nice "<<name<<" you got that right\n";
                     cout<<"Your score is: "<<score<<endl;
+                    introline();
 
                 }
             }
             
             if (match == false) {
                 lives--;
-                cout<<"Yikes! Be careful you got that wrong\n";
-                cout<<"You have "<<lives<<" left\n";
+                cout<<"Yikes! Be careful "<<name<<" got that wrong\n";
+                cout<<"You have "<<lives<<" lives left\n";
             }
             
             if (correct == limit) {
@@ -77,6 +99,7 @@ class Game
 
     }
     friend void wordle();
+    friend void wordleplay();
 
 
 
@@ -84,31 +107,25 @@ class Game
 
 class LV1
 {
-    string words[2]= {"tale","late"};
+    string word[2]= {"tale","late"};
     public:
     LV1(){
-        Game lv1(0,2,2,words);
+        Game lv1(0,2,2,word);
         lv1.play();
         }  
    
 };
 class LV2
 {
-    string words[3] = {"rain","rani","rnai"};
+    string lab[6] = {"opts","tops","spot","pots","post","stop"};
     public:
     LV2()
     {
-        Game lv2(0,3,3,words);
+        Game lv2(0,6,6,lab);
         lv2.play();
     }
 };
-void introline()
-{
-    for(int i=0;i<139;i++)
-    {
-        cout<<"-";
-    }
-}
+
 void lvlmenu()
 {
     cout<<"LV: 1\n";
@@ -116,10 +133,29 @@ void lvlmenu()
     cout<<"LV: 3\n";
     cout<<"LV: 4\n";
 }
+void wordleplay()
+{
+     leveldisplay();
+    LV1 abc;
+    cout<<"\n";
+    introline();
+    cout<<"\n"; 
+    if(lives!=0)
+    {   
+        lvlupdate++;
+        leveldisplay();
+        LV2 bac;
+    }
+    else
+    {
+        cout<<name<<" your total score is: "<<score;
+    }
+}
 
 void wordle()
 {
-     introline();
+    int press = 0;
+    introline();
     cout<<"\n";
     cout<<"\t\t\t\t\t\t\tWELCOME TO WORDLY"<<endl;
     cout<<"\t\t\t\t\t\tThe best guessing game on net\n";
@@ -129,21 +165,34 @@ void wordle()
     introline();
     cout<<"\n";
     lvlmenu();
-    cout<<"\nWelcome to LV1\n";
-    LV1 abc;
-
-    cout<<"\nWelcome to LV2\n";
-     LV2 bac;
+    cout<<"What is your name\n";
+    cin>>name;
+    cout<<"1)Play\n";
+    cout<<"2)Check high score\n\n";
     
-    
-    
+    cout<<"Press 1 or 2\n";
+    cin>>press;
+    if(press==1)
+    {
+        wordleplay();
+    }
+    else if(press == 2)
+    {
+        //add the highscore file
+    }
+    else
+    {
+        cout<<"Invalid option! try again\n";
+        wordle();
+    }
 
 }
+
 
 int main()
 {
     
-    wordle();
+     wordle();
 
     
     
